638a3f46a72a9abaf989ab9479370da24c0eade3
Omsber <xyzsyx@163.com>
2022-06-28 17:30:11 +0800

    feat: getters中加入楼栋和楼层

--- src/store/getters.js
+++ src/store/getters.js
@@ -22,6 +22,8 @@ export default {
   regulatoryUnits: state => state.common.regulatoryUnits,
   parkTree: state => state.common.parkTree,
   parksForSelect: state => state.common.parksForSelect,
+  buildingsForSelect: state => state.common.buildingsForSelect,
+  floorTree: state => state.common.floorTree,
   organizationTree: state => state.common.organizationTree,
   roleTree: state => state.common.roleTree,
   currentParkTreeKeySelected: state => state.common.currentParkTreeKeySelected,
