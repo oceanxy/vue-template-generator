1579ce1d125b3385f3b0b055d78c1539d1a7cca9
张桂苓 <zhgling@lanjor.cn>
2022-06-30 18:45:28 +0800

    添加静态数据

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   // mock数据开关。开发模式下生效
-  mock: false,
+  mock: true,
   // 请求超时时间
   timeout: 30000,
   // mock请求延迟时间
@@ -11,5 +11,6 @@ export default {
   // 全局消息最大显示个数
   maxMessageCount: 1,
   // 账号密码加密key
-  publicKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCs7Iu8OPMKCt38fCWV5PdA7+TA+vxgNFnAiC+9xw8F4JifCKNRg07w3zxbSoUmW7dN3NMubME9hQQizmx7IJk3hn91ieVg+CiYdA9MwpEThezYPsJ6+Oj9RsVPOCAsXa5+XRlc1lbmo7b21n5SVSkbog2OMqB2OlZK+SdwY+vrhQIDAQAB'
+  publicKey:
+    'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCs7Iu8OPMKCt38fCWV5PdA7+TA+vxgNFnAiC+9xw8F4JifCKNRg07w3zxbSoUmW7dN3NMubME9hQQizmx7IJk3hn91ieVg+CiYdA9MwpEThezYPsJ6+Oj9RsVPOCAsXa5+XRlc1lbmo7b21n5SVSkbog2OMqB2OlZK+SdwY+vrhQIDAQAB'
 }
--- src/views/client/basis/Contract/index.jsx
+++ src/views/client/basis/Contract/index.jsx
@@ -24,7 +24,7 @@ export default {
             <Descriptions.Item label="租用场所">珠光御景 / 南区27栋 / 606号</Descriptions.Item>
             <Descriptions.Item label="合同期限">2022-05-18 至 2023-05-17（5年）</Descriptions.Item>
             <Descriptions.Item label="生成时间">2022-05-18 19:51</Descriptions.Item>
-            <Descriptions.Item label="签约状态">已签约（签约时间：2022-05-18 19:51，经办人：陈永森,13883192629 ）</Descriptions.Item>
+            <Descriptions.Item label="签约状态">已签约（签约时间：2022-05-18 19:51，经办人：李卢,15865854254 ）</Descriptions.Item>
             <Descriptions.Item>
               <div class="contract-status">签约审核中，请耐心等待</div>
             </Descriptions.Item>
@@ -45,10 +45,74 @@ export default {
           contentClass="contract-item-content"
         >
           <Descriptions column={1}>
-            <Descriptions.Item label="租用场所">珠光御景 / 南区27栋 / 606号</Descriptions.Item>
+            <Descriptions.Item label="租用场所">珠光御景 / 南区29栋 / 546号</Descriptions.Item>
             <Descriptions.Item label="合同期限">2022-05-18 至 2023-05-17（5年）</Descriptions.Item>
             <Descriptions.Item label="生成时间">2022-05-18 19:51</Descriptions.Item>
-            <Descriptions.Item label="签约状态">已签约（签约时间：2022-05-18 19:51，经办人：陈永森,13883192629 ）</Descriptions.Item>
+            <Descriptions.Item label="签约状态">已签约（签约时间：2022-05-18 19:51，经办人：陈强,15232635585 ）</Descriptions.Item>
+            <Descriptions.Item>
+              <div class="contract-status">签约审核中，请耐心等待</div>
+            </Descriptions.Item>
+            <Descriptions.Item>
+              <Space>
+                <Button type="primary" ghost class="preview">
+                  <Icon component={download} />
+                  合同下载
+                </Button>
+                <Button type="primary" ghost class="preview">
+                  <Icon component={renew} />
+                  我要续约
+                </Button>
+                <Button type="primary" ghost class="preview">
+                  <Icon component={terminateContract} width='0.5em' />
+                  申请解约
+                </Button>
+              </Space>
+            </Descriptions.Item>
+          </Descriptions>
+        </BNContainer>
+        <BNContainer
+          moduleTitle=" 21423423D丨企业入驻合同"
+          class="contract-item"
+          titleClass="contract-item-title"
+          contentClass="contract-item-content"
+        >
+          <Descriptions column={1}>
+            <Descriptions.Item label="租用场所">珠光御景壹号 / 北区07栋 / 26号</Descriptions.Item>
+            <Descriptions.Item label="合同期限">2022-05-18 至 2023-05-17（5年）</Descriptions.Item>
+            <Descriptions.Item label="生成时间">2022-05-18 19:51</Descriptions.Item>
+            <Descriptions.Item label="签约状态">已签约（签约时间：2022-05-18 19:51，经办人：陈永森,13883191521 ）</Descriptions.Item>
+            <Descriptions.Item>
+              <div class="contract-status">签约审核中，请耐心等待</div>
+            </Descriptions.Item>
+            <Descriptions.Item>
+              <Space>
+                <Button type="primary" ghost class="preview">
+                  <Icon component={download} />
+                  合同下载
+                </Button>
+                <Button type="primary" ghost class="preview">
+                  <Icon component={renew} />
+                  我要续约
+                </Button>
+                <Button type="primary" ghost class="preview">
+                  <Icon component={terminateContract} width='0.5em' />
+                  申请解约
+                </Button>
+              </Space>
+            </Descriptions.Item>
+          </Descriptions>
+        </BNContainer>
+        <BNContainer
+          moduleTitle=" 21423423D丨企业入驻合同"
+          class="contract-item"
+          titleClass="contract-item-title"
+          contentClass="contract-item-content"
+        >
+          <Descriptions column={1}>
+            <Descriptions.Item label="租用场所">珠光御景山水城 / 17栋 / 606号</Descriptions.Item>
+            <Descriptions.Item label="合同期限">2022-05-18 至 2023-05-17（5年）</Descriptions.Item>
+            <Descriptions.Item label="生成时间">2022-05-18 19:51</Descriptions.Item>
+            <Descriptions.Item label="签约状态">已签约（签约时间：2022-05-18 19:51，经办人：张思雨,13852658547 ）</Descriptions.Item>
             <Descriptions.Item>
               <div class="contract-status">签约审核中，请耐心等待</div>
             </Descriptions.Item>
