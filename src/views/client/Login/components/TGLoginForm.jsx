2efdc107f220f9788c17dd0340ab6349899c4088
yangjialong <1476927892@qq.com>
2022-07-04 18:24:39 +0800

    更新前台登录Key

--- src/views/client/Login/components/TGLoginForm.jsx
+++ src/views/client/Login/components/TGLoginForm.jsx
@@ -13,7 +13,7 @@ export default Form.create({ name: 'TGLoginForm' })({
     if (process.env.NODE_ENV === 'development') {
       // 开发模式默认账号密码
       this.form.setFieldsValue({
-        username: 'sysadmin',
+        username: '15826032171',
         password: '123456',
         picCode: 'LANJOR'
       })
@@ -25,7 +25,7 @@ export default Form.create({ name: 'TGLoginForm' })({
     handleSubmit(e) {
       e.preventDefault()
 
-      this.form.validateFields(async(err, values) => {
+      this.form.validateFields(async (err, values) => {
         if (!err) {
           const { status } = await this.login(values)
 
@@ -41,70 +41,39 @@ export default Form.create({ name: 'TGLoginForm' })({
   },
   render() {
     return (
-      <Form
-        form={this.form}
-        class="bn-login-form"
-        onSubmit={this.handleSubmit}
-      >
+      <Form form={this.form} class="bn-login-form" onSubmit={this.handleSubmit}>
         <Form.Item>
-          {
-            this.form.getFieldDecorator('username', {
-              rules: [{ required: true, message: '请输入用户名!', trigger: '' }]
-            })(
-              <Input placeholder="请输入账号">
-                <template slot="prefix">
-                  <a-icon
-                    style={{ color: '#1890ff' }}
-                    type="user"
-                  />
-                </template>
-              </Input>
-            )
-          }
+          {this.form.getFieldDecorator('username', {
+            rules: [{ required: true, message: '请输入用户名!', trigger: '' }]
+          })(
+            <Input placeholder="请输入账号">
+              <template slot="prefix">
+                <a-icon style={{ color: '#1890ff' }} type="user" />
+              </template>
+            </Input>
+          )}
         </Form.Item>
         <Form.Item>
-          {
-            this.form.getFieldDecorator('password', { rules: [{ required: true, message: '请输入密码!' }] })(
-              <Input
-                placeholder="请输入密码"
-                type="password"
-              >
-                <template slot="prefix">
-                  <Icon
-                    style={{ color: '#1890ff' }}
-                    type="lock"
-                  />
-                </template>
-              </Input>
-            )
-          }
+          {this.form.getFieldDecorator('password', { rules: [{ required: true, message: '请输入密码!' }] })(
+            <Input placeholder="请输入密码" type="password">
+              <template slot="prefix">
+                <Icon style={{ color: '#1890ff' }} type="lock" />
+              </template>
+            </Input>
+          )}
         </Form.Item>
         <Form.Item class="code">
-          {
-            this.form.getFieldDecorator('picCode', { rules: [{ required: true, message: '请输入验证码!' }] })(
-              <Input placeholder="请输入验证码">
-                <template slot="prefix">
-                  <Icon
-                    style={{ color: '#1890ff' }}
-                    type="code"
-                  />
-                </template>
-              </Input>
-            )
-          }
-          <img
-            src={this.picCodePath}
-            alt=""
-            onClick={this.genCode}
-          />
+          {this.form.getFieldDecorator('picCode', { rules: [{ required: true, message: '请输入验证码!' }] })(
+            <Input placeholder="请输入验证码">
+              <template slot="prefix">
+                <Icon style={{ color: '#1890ff' }} type="code" />
+              </template>
+            </Input>
+          )}
+          <img src={this.picCodePath} alt="" onClick={this.genCode} />
         </Form.Item>
         <Form.Item>
-          <Button
-            class='login-submit'
-            loading={this.loading}
-            htmlType="submit"
-            type="primary"
-          >
+          <Button class="login-submit" loading={this.loading} htmlType="submit" type="primary">
             立即登录
           </Button>
         </Form.Item>
