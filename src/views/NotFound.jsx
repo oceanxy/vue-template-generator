0d94ebd6da26bfa9285d10f3d97fae9f3f3e9d4e
Omsber <xyzsyx@163.com>
2023-01-05 11:13:48 +0800

    feat: 404页面更换图片

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,5 +1,5 @@
 import TGHeader from '@/layouts/components/TGHeader'
-import { Button, Layout } from 'ant-design-vue'
+import { Button, Layout, Space } from 'ant-design-vue'
 
 export default {
   name: 'NotFound',
@@ -8,8 +8,10 @@ export default {
       <Layout class={'tg-not-found'}>
         <TGHeader layout="manager" page={'not-found'} />
         <Layout class={'tg-not-found-content'}>
-          <div class={'hint'}>页面走丢了~</div>
-          <Button type={'link'} onClick={() => this.$router.replace('/')}>返回首页</Button>
+          <Space class={'hint'}>
+            <span>页面走丢了~</span>
+            <Button type={'link'} onClick={() => this.$router.replace('/')}>返回首页</Button>
+          </Space>
         </Layout>
       </Layout>
     )
