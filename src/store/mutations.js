212d19bab3c41a2c78ef86d1f2730b258dfa658b
Omsber <xyzsyx@163.com>
2024-04-29 17:58:44 +0800

    refactor(mutations): 优化setState注释

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -195,7 +195,7 @@ export default {
   /**
    * 设置数据
    * @param state {Object}
-   * @param value {Array || Object} 设置的值
+   * @param value {Array | Object} 设置的值
    * @param moduleName {string} 模块名
    * @param [submoduleName] {string} 子模块名
    * @param stateName {string} 需要设置的字段，默认 state.details
